0df7e86881133cfd28c6a20a8e3fd2d1d018f8db
suman <77767123+suman-aws@users.noreply.github.com>
2022-04-05 09:59:50 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,10 @@
+import sys 
 model = ['INT']
 envi = ['Apple']
+stdoutOrigin=sys.stdout 
+sys.stdout = open("model_list.txt", "w")
+
 # for i in range(len(model)):
 print (str(model)+'='+str(envi))
+sys.stdout.close()
+sys.stdout=stdoutOrigin
