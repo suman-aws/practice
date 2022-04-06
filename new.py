90188eea6a3ac1e7f8e41621cd33bee01013e7d1
suman <77767123+suman-aws@users.noreply.github.com>
2022-04-06 15:12:19 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,11 +2,10 @@ model=['Apple','Water']
 version=[1.0,2.0]
 hsiuid=[1,2]
 tableName=['Mango','Tango','Charlie','delta']
-# for i in range(len(model)):
-#     print(str(model[i])+'='+str(version[i])+'='+str(hsiuid[i]))
-print(model)
-print(version)
-print(hsiuid)
-
+model_details = []
+model_details.append(model)
+model_details.append(version)
+model_details.append(hsiuid)
+print(model_details)
 
 
