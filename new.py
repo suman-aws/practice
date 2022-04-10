45492e9eb03777ff79140ddcd7a6c3e852218484
suman <77767123+suman-aws@users.noreply.github.com>
2022-04-10 07:44:10 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,15 +4,15 @@ version=[1.0,2.0]
 hsiuid=[1,2]
 tableName=['Mango','Tango','Charlie','delta']
 model_details = []
-for i in range(len(model)):
-  print(envir[i]+'='+model[i]+'='+str(version[i]))
+# for i in range(len(model)):
+#   print(envir[i]+'='+model[i]+'='+str(version[i]))
 # print(model)
 # print(envi)
 # print(version)
 
-# print('model details:',model)
-# print('environment:',envi)
-# print('version details:',version)
+print("model details:",model)
+print("environment:",envi)
+print("version details:",version)
 
 # for i in range(len(model)):
 #     # print(str(model[i])+'='+str(version[i])+'='+str(hsiuid[i]))
