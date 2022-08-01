cb25434bbae2f3bfcab9dbaa3ecf71db990c9f0e
suman <77767123+suman-aws@users.noreply.github.com>
2022-08-01 11:56:19 +0530

    Update hs.py

--- hs.py
+++ hs.py
@@ -2,19 +2,19 @@ import sys
 n = len(sys.argv[1])
 envir = sys.argv[1][1 : n - 1]
 envir = envir.split(", ")
-
-# e = []
-host = []
-for i in range(len(envir)):
-    e = envir[i].split(" ")
-for i in range(len(e)):
-    f = e[i].split(",")
-# print(str(f))
-if "INT" in f:
-    host.append('abc.host.7')
-elif "QA" in f:
-    host.append('abc.host.3')
-elif "PROD" in f:
-    host.append('abc.host.5')
+print(envir)
+# # e = []
+# host = []
+# for i in range(len(envir)):
+#     e = envir[i].split(" ")
+# for i in range(len(e)):
+#     f = e[i].split(",")
+# # print(str(f))
+# if "INT" in f:
+#     host.append('abc.host.7')
+# elif "QA" in f:
+#     host.append('abc.host.3')
+# elif "PROD" in f:
+#     host.append('abc.host.5')
     
-print(str(host))
+# print(str(host))
