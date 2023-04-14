395b18f149898b9aa2bc4113093380a8bf155924
suman-aws <ss.devops@outlook.com>
2023-04-14 06:58:27 +0530

    updated on qa

--- hs.py
+++ hs.py
@@ -1,24 +1,31 @@
+import subprocess
 import os
 
-# Replace this with the Python code that generates your output
-output = "['M1','M2'] [V1,V2] ['QA','QA']"
+# Run the script and capture the output
+script_path = r"C:\Users\hp\Desktop\New folder\practice\prepare_re_deployment.py"
+result = subprocess.run(['python', script_path], capture_output=True)
 
-# Parse the output
-model_names, model_versions, deployment_contexts = [], [], []
-for value in output.split():
-    if value.startswith("['"):
-        model_names.append(value.strip("[,']"))
-    elif value.startswith("[") and value.endswith("]"):
-        model_versions.append(value.strip("[]"))
-    elif value.startswith("'") and value.endswith("'"):
-        deployment_contexts.append(value.strip("'"))
+# Parse the output into variables
+output = result.stdout.decode('utf-8').split('\n')
+env = output[0].split(': ')[1].split(', ')
+deployment_context = output[1].split(': ')[1].split(', ')
+model_names = output[2].split(': ')[1].split(', ')
+model_versions = output[3].split(': ')[1].split(', ')
+system_uids = output[4].split(': ')[1].split(', ')
+testing_time = output[5].split(': ')[1].split(', ')
 
-# Set the environment variables for use in GitHub Actions
-os.environ["MODEL_NAMES"] = ",".join(model_names)
-os.environ["MODEL_VERSIONS"] = ",".join(model_versions)
-os.environ["DEPLOYMENT_CONTEXTS"] = ",".join(deployment_contexts)
+# Set the environment variables for the GitHub workflow
+os.environ['ENVIRONMENT'] = ', '.join(env)
+os.environ['DEPLOYMENT_CONTEXT'] = ', '.join(deployment_context)
+os.environ['MODEL_NAMES'] = ', '.join(model_names)
+os.environ['MODEL_VERSIONS'] = ', '.join(model_versions)
+os.environ['SYSTEM_UIDS'] = ', '.join(system_uids)
+os.environ['TESTING_TIME'] = ', '.join(testing_time)
 
-# Print the parsed values for verification
-print("Model Names:", model_names)
-print("Model Versions:", model_versions)
-print("Deployment Contexts:", deployment_contexts)
+# Print the environment variables for verification
+print(os.environ['ENVIRONMENT'])
+print(os.environ['DEPLOYMENT_CONTEXT'])
+print(os.environ['MODEL_NAMES'])
+print(os.environ['MODEL_VERSIONS'])
+print(os.environ['SYSTEM_UIDS'])
+print(os.environ['TESTING_TIME'])
