42e54ae3fb6c1af743a6eb257c343ee4abb3bb20
suman-aws <ss.devops@outlook.com>
2023-04-14 09:02:37 +0530

    updated

--- hs.py
+++ hs.py
@@ -1,37 +1,29 @@
+#!/usr/bin/env python3
+
 import subprocess
-import os
 
-# Run the script and capture the output
-result = subprocess.run(["python", "prepare_re_deployment.py"], capture_output=True, text=True)
-import os
+output = subprocess.check_output(["python3", "prepare_new_deployment.py"]).decode().split("\n")
 
-# input string
-input_str = '''QA=RW=T-PR=1.0=77276535=20230218-235959
-QA=RW+RS=LR=1.0=77276535=20230218-235959'''
+for line in output:
+  parts = line.split("=")
 
-# initialize empty lists
-env_list = []
-deployment_context_list = []
-model_names_list = []
-model_versions_list = []
-system_uids_list = []
-testing_times_list = []
+  for j in range(6):
+    if j == 0:
+      env = parts[j]
+    elif j == 1:
+      context = parts[j]
+    elif j == 2:
+      model = parts[j]
+    elif j == 3:
+      version = parts[j]
+    elif j == 4:
+      sysuid = parts[j]
+    elif j == 5:
+      testingtime = parts[j]
 
-# split the input string by newline character and iterate over each line
-for line in result.split('\n'):
-    # split the line by '=' character and append the values to the corresponding lists
-    parts = line.split('=')
-    env_list.append(parts[0])
-    deployment_context_list.append(parts[1])
-    model_names_list.append(parts[2])
-    model_versions_list.append(parts[3])
-    system_uids_list.append(parts[4])
-    testing_times_list.append(parts[5])
-
-# set the values as environment variables in the GitHub Actions workflow
-os.environ['ENVironment'] = str(env_list)
-os.environ['DEPLOYMENT_CONTEXT'] = str(deployment_context_list)
-os.environ['MODEL_NAMES'] = str(model_names_list)
-os.environ['MODEL_VERSIONS'] = str(model_versions_list)
-os.environ['SYSTEM_UIDS'] = str(system_uids_list)
-os.environ['TESTING_TIMES'] = str(testing_times_list)
+  print("Env:", env)
+  print("Deployment Context:", context)
+  print("Model Names:", model)
+  print("Model Versions:", version)
+  print("SystemUIDs:", sysuid)
+  print("TestingTime:", testingtime)
