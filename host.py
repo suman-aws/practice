8ff465c977a9626c5ff3eecfb56b02537174b59a
suman-aws <ss.devops@outlook.com>
2023-04-21 15:43:38 +0530

    updated on g

--- host.py
+++ host.py
@@ -1,10 +1,14 @@
 import sys
-output = sys.argv[1]
-lines = output.splitlines()
-DEPLOYMENT_CONTEXT = '';
-for line in lines:
-    # print(line.split(': ')[1])
-    if 'DEPLOYMENT_CONTEXT' in line:
-        DEPLOYMENT_CONTEXT=line.split(': ')[1];
+env = sys.argv[1]
+context = sys.argv[2]
+models = sys.argv[3]
+# lines = output.splitlines()
+# DEPLOYMENT_CONTEXT = '';
+# for line in lines:
+#     # print(line.split(': ')[1])
+#     if 'DEPLOYMENT_CONTEXT' in line:
+#         DEPLOYMENT_CONTEXT=line.split(': ')[1];
        
-print(DEPLOYMENT_CONTEXT)
+print(env)
+print(context)
+print(models)
