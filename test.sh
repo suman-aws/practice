a7a3077201c569afa54bdfee6139a8cb9189361e
suman-aws <ss.devops@outlook.com>
2023-04-27 19:29:26 +0530

    updated on

--- test.sh
+++ test.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 output=$(python prepare_new_deployment.py)
-
-environment=$(echo "$output" | awk -F ': ' '/ENVironment:/ {print $2}')
-deployment_context=$(echo "$output" | awk -F ': ' '/DEPLOYMENT_CONTEXT:/ {print $2}')
-model_names=$(echo "$output" | awk -F ': ' '/MODEL_NAMES:/ {print $2}')
-model_versions=$(echo "$output" | awk -F ': ' '/MODEL_VERSIONS:/ {print $2}')
-system_uids=$(echo "$output" | awk -F ': ' '/SYSTEM_UIDS:/ {print $2}')
-testing_times=$(echo "$output" | awk -F ': ' '/TESTING_TIMES:/ {print $2}')
-
-echo "environment: $environment"
-echo "deployment_context: $deployment_context"
-echo "model_names: $model_names"
-echo "model_versions: $model_versions"
-echo "system_uids: $system_uids"
-echo "testing_times: $testing_times"
+environment=$(echo "$output" | grep -oP "ENVironment: \[\K[^\]]+" | awk '{print $1}')
+deployment_context=$(echo "$output" | grep -oP "DEPLOYMENT_CONTEXT: \[\K[^\]]+" | awk '{print $1}')
+model_names=$(echo "$output" | grep -oP "MODEL_NAMES: \[\K[^\]]+" | awk '{print $1}')
+model_versions=$(echo "$output" | grep -oP "MODEL_VERSIONS: \[\K[^\]]+" | awk '{print $1}')
+system_uids=$(echo "$output" | grep -oP "SYSTEM_UIDS: \[\K[^\]]+" | awk '{print $1}')
+testing_times=$(echo "$output" | grep -oP "TESTING_TIMES: \[\K[^\]]+" | awk '{print $1}')
+echo $deployment_context
