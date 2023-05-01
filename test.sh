36a8f393514dd2a19aa8bcf8193c6e8beb6f95b6
suman-aws <ss.devops@outlook.com>
2023-05-01 17:25:16 +0530

    updated on models

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-output=$(python prepare_new_deployment.py)
-environment=$(echo "$output" | grep -oP "ENVironment: \[\K[^\]]+" | awk '{print $1}')
-deployment_context=$(echo "$output" | grep -oP "DEPLOYMENT_CONTEXT: \[\K[^\]]+" | awk '{print $1}')
-model_names=$(echo "$output" | grep -oP "MODEL_NAMES: \[\K[^\]]+" | awk '{print $1}')
-model_versions=$(echo "$output" | grep -oP "MODEL_VERSIONS: \[\K[^\]]+" | awk '{print $1}')
-system_uids=$(echo "$output" | grep -oP "SYSTEM_UIDS: \[\K[^\]]+" | awk '{print $1}')
-testing_times=$(echo "$output" | grep -oP "TESTING_TIMES: \[\K[^\]]+" | awk '{print $1}')
-echo $deployment_context
+DEPLOYMENT_CONTEXT=$1
+MODEL_NAMES=$2
+MODEL_VERSIONS=$3
+SYSTEM_UIDS=$4
+TESTING_TIMES=$5
+
+echo "Deployment Context: $DEPLOYMENT_CONTEXT"
+echo "Model Names: $MODEL_NAMES"
+echo "Model Versions: $MODEL_VERSIONS"
+echo "System UIDs: $SYSTEM_UIDS"
+echo "Testing Times: $TESTING_TIMES"
+
+# rest of deployment script
