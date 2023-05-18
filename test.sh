5a3bdf22bc3ecf8f17287b7fc7daa2648bbc3bde
suman-aws <ss.devops@outlook.com>
2023-05-18 07:27:25 +0530

    docker image added

--- test.sh
+++ test.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-env=("QA" "QA")
+# env=("QA" "QA")
 
-if [ "$env[0]" = "QA" ]; then
-  host="abc5.com"
-elif [ "$env[0" = "PROD" ]; then
-  host="abc3.com"
-else
-  host="abc7.com"
-fi
+# if [ "$env[0]" = "QA" ]; then
+#   host="abc5.com"
+# elif [ "$env[0" = "PROD" ]; then
+#   host="abc3.com"
+# else
+#   host="abc7.com"
+# fi
 
-echo "$host"
+# echo "$host"
+i
 
-# DEPLOYMENT_CONTEXT=$1
-# MODEL_NAMES=$2
-# MODEL_VERSIONS=$3
-# SYSTEM_UIDS=$4
-# TESTING_TIMES=$5
+DEPLOYMENT_CONTEXT=$1
+MODEL_NAMES=$2
+MODEL_VERSIONS=$3
+SYSTEM_UIDS=$4
+TESTING_TIMES=$5
 
-# echo "Deployment Context: $DEPLOYMENT_CONTEXT"
-# echo "Model Names: $MODEL_NAMES"
-# echo "Model Versions: $MODEL_VERSIONS"
-# echo "System UIDs: $SYSTEM_UIDS"
-# echo "Testing Times: $TESTING_TIMES"
+echo "Deployment Context: $DEPLOYMENT_CONTEXT"
+echo "Model Names: $MODEL_NAMES"
+echo "Model Versions: $MODEL_VERSIONS"
+echo "System UIDs: $SYSTEM_UIDS"
+echo "Testing Times: $TESTING_TIMES"
+
+siuid=(python --version)
+echo $siuid
 
 # # rest of deployment script
